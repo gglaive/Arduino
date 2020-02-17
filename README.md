61102df3daea2cdfbc4b09309261201a6085d25a
Yrrieth <thierry.wen@laposte.net>
2020-02-17 20:47:49 +0100

    Maj morse

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Arduino
 
+TODO
 
+Ecrire au clavier sur Python et l'envoyer vers Arduino et les retranscrire sous forme de signal sonore ou lumineux.
--- morse.py
+++ morse.py
@@ -3,35 +3,96 @@ import time
 
 # set up the serial line
 ser = serial.Serial('COM6', 9600)
-time.sleep(2)
-
-# Read and record the data
 data =[]	   # empty list to store the data
-for i in range(50):
-	b = ser.readline()	 # read a byte string
-	string_n = b.decode()  # decode byte string into Unicode  
+
+def translate(word):
+	if (word == "01"):
+		print("A")
+	if (word == "1000"):
+		print("B")
+	if (word == "1010"):
+		print("C")
+	if (word == "100"):
+		print("D")
+	if (word == "0"):
+		print("E")
+	if (word == "0010"):
+		print("F")
+	if (word == "110"):
+		print("G")
+	if (word == "0000"):
+		print("H")
+	if (word == "00"):
+		print("I")
+	if (word == "0111"):
+		print("J")
+	if (word == "101"):
+		print("K")
+	if (word == "0100"):
+		print("L")
+	if (word == "11"):
+		print("M")
+	if (word == "10"):
+		print("N")
+	if (word == "111"):
+		print("O")
+	if (word == "0110"):
+		print("P")
+	if (word == "1101"):
+		print("Q")
+	if (word == "010"):
+		print("R")
+	if (word == "000"):
+		print("S")
+	if (word == "1"):
+		print("T")
+	if (word == "001"):
+		print("U")
+	if (word == "0001"):
+		print("V")
+	if (word == "011"):
+		print("W")
+	if (word == "1001"):
+		print("X")
+	if (word == "1011"):
+		print("Y")
+	if (word == "1100"):
+		print("Z")
+	if (word == "01"):
+		print("A")
+	if (word == "11111"):
+		print("0")
+	if (word == "01111"):
+		print("1")
+	if (word == "00111"):
+		print("2")
+	if (word == "00011"):
+		print("3")
+	if (word == "00001"):
+		print("4")
+	if (word == "00000"):
+		print("5")
+	if (word == "10000"):
+		print("6")
+	if (word == "11000"):
+		print("7")
+	if (word == "11100"):
+		print("8")
+	if (word == "11110"):
+		print("9")
+	
+while 1:
+	value = ser.readline()
+	string_n = value.decode()  # decode byte string into Unicode  
 	string = string_n.rstrip() # remove \n and \r
-	try:
-		flt = float(string)	# convert string to float
-		print(flt)
-		data.append(flt)	   # add to the end of data list
-	except:
-		pass
-	time.sleep(0.1)	# wait (sleep) 0.1 seconds
-
-ser.close()
-
-'''
-while True:
-	sw = board.digital[10].read()
-	if sw is True:
-		board.digital[13].write(1)
+	#print(type(string))
+	print(string)
+	if (string == "2"):
+		mot = ""
+		for i in range(len(data)):
+			mot = mot + data[i]
+		print("/////////////////\n")
+		translate(mot)
+		data.clear()
 	else:
-		board.digital[13].write(0)
-	time.sleep(0.1)
-'''
-
-# show the data
-
-for line in data:
-	print(line)
+		data.append(string)
