e6ef1b7aca4867d2368ad6221cecb63216300246
Yrrieth <thierry.wen@laposte.net>
2020-03-02 19:41:52 +0100

    Ajout fichier Morse Python vers Arduino

---
+++ morse_py_ard.ino
@@ -0,0 +1,34 @@
+byte receivedChar;
+boolean newData = false;
+
+int led = 9; 
+
+void setup() {
+ Serial.begin(9600);
+ pinMode(led, OUTPUT);
+}
+
+void loop() {
+  
+
+    receivedChar = Serial.read();
+    //Serial.println(Serial.read());
+    
+    
+    digitalWrite(led, LOW);
+    delay(100);
+    
+    if (receivedChar == '1') {
+      digitalWrite(led, HIGH);
+      delay(1000);
+    }
+    if (receivedChar == '0') {
+      digitalWrite(led, HIGH);
+      delay(100);
+    }
+    if (receivedChar == '2') {
+      digitalWrite(led, LOW);
+      delay(500);
+    }
+    
+}
